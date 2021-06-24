8b90bb2eb6b226761f0b678017582c5e20a7bdb3
liuxing2020 <liux_xing1998@163.com>
2021-06-24 14:20:24 +0800

    first commit

---
+++ CBS2/db/02_params.sql
@@ -0,0 +1,12 @@
+-- T1223412_CBSA213134naisdf你撒打发士大夫啊手动阀手动阀
+
+--asfdasdf
+
+DELETE FROM FMDBUN.SFIASDF WHERE ASDF IN ASDFSADFASDF
+IMPORT FROM 'DSAFASDFAFASDF.DFEL' OF DELETEASDFASD
+IMPORT FROM 'FASDFASfasdf.del' asdfasdfasd
+sadfasdfasfdsa
+asdfasdfasdasdf
+asdfasdfasdfasdf
+adf
+sadfasdfasfdsa
